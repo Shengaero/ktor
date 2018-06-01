e6fc094635464925886d71c8c358ab6c11df65fd
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-06-01 17:38:09 +0300

    Make ktor-client-core common

--- ktor-utils/ktor-utils-jvm/src/io/ktor/cio/Readers.kt
+++ ktor-utils/ktor-utils-jvm/src/io/ktor/cio/Readers.kt
@@ -1,10 +1,25 @@
 package io.ktor.cio
 
 import kotlinx.coroutines.experimental.io.*
-import kotlinx.io.core.*
+import kotlinx.io.pool.*
+import java.io.*
 import java.nio.*
 
-suspend fun ByteReadChannel.toByteArray(limit: Int = Int.MAX_VALUE): ByteArray = readRemaining(limit.toLong()).readBytes()
+suspend fun ByteReadChannel.toByteArray(sizeHint: Int = 0, pool: ObjectPool<ByteBuffer> = KtorDefaultPool): ByteArray {
+    val result = ByteArrayOutputStream(sizeHint)
+    pool.use { buffer ->
+        while (!isClosedForRead) {
+            buffer.clear()
+            val count = readAvailable(buffer)
+            if (count == -1) break
+            buffer.flip()
+
+            result.write(buffer.array(), buffer.arrayOffset() + buffer.position(), count)
+        }
+    }
+
+    return result.toByteArray()
+}
 
 suspend fun ByteReadChannel.pass(buffer: ByteBuffer, block: suspend (ByteBuffer) -> Unit) {
     while (!isClosedForRead) {
